0c7a6ad22f15ee22e32c628c60ed9907612d83d8
Tareq Alotaibi <tareqdev@protonmail.com>
2019-12-16 17:08:33 +0300

    video show route

--- homedia-api/Route/show.js
+++ homedia-api/Route/show.js
@@ -1,6 +1,25 @@
 const express = require('express')
 const route = express.Router()
 
+// import model
+const Video = require('../Model/Video')
+
+route.get('/video' , (req,res) => {
+
+  Video.find()
+  .then(data => {
+    res.json(data)
+  })
+.catch(err =>  {
+  res.json(err)
+})
+
+})
+
+
+
+
+
 
 
 
