c41a76b29c54f659941302e457a451131dcc2712
Tareq Alotaibi <tareqdev@protonmail.com>
2019-12-16 23:11:29 +0300

    added Uploading functionalty

--- homedia-api/server.js
+++ homedia-api/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+var cors = require('cors')
 
 // App
 const app = express()
@@ -18,8 +19,10 @@ const musicRoute = require('./Route/music')
 
 
 // Middlewares
+app.use(cors())
 app.use(express.urlencoded({extended:false}))
-app.use(express.json())
+app.use(express.json({limit: '50gb', extended: true}));
+
 
 app.use('/show' , showRoute)
 app.use('/users' , usersRoute)
--- homedia-front/src/component/admin/UploadVideo.jsx
+++ homedia-front/src/component/admin/UploadVideo.jsx
@@ -1,18 +1,84 @@
 import React from 'react'
 import { Grid, Segment , Form , Button } from 'semantic-ui-react'
-
+import axios from 'axios'
 class UploadVideo extends React.Component {
+
+state={
+  path:null,
+  poster:null,
+  video:null
+}
   render () {
 
 
 
-let onChangeHandler =event=> {
 
-          console.log(event.target.files[0])
+let submitHondler = () => {
 
+      axios.post('http://localhost:62300/video/new', {
+
+          name:this.state.path,
+          posterpath:this.state.poster,
+          filepath:this.state.video
+
+      })
+      .then(data => {
+        console.log(data);
+      }).catch(err => {
+        console.log(err);
+      })
+
+      console.log(this.state.path);
+      console.log("Poster",this.state.poster);
+      console.log("video" , this.state.video);
+
+}
+
+
+let onChangeHandlerName = e => {
+    this.setState({
+      path:e.target.value
+    })
+}
+
+
+
+
+
+
+let onChangeHandlerVideo = e =>{
+
+  let video = e.target.files
+  let reader = new FileReader()
+  reader.readAsDataURL(video[0])
+
+  reader.onload =(e)=>{
+    this.setState({
+      video:e.target.result
+    })
+  }
+
+}
+let onChangeHandlerPoster = e =>{
+  let poster = e.target.files
+  let reader = new FileReader()
+  reader.readAsDataURL(poster[0])
+  reader.onload = (e)=> {
+
+      this.setState({
+          poster:e.target.result
+      })
+
+
+  }
 
 
 }
+
+
+
+
+
 
 
           return(
@@ -30,16 +96,12 @@ let onChangeHandler =event=> {
 
           <div className="CenterForm">
 
-                    <Form>
-                        <Form.Field>
-                          <label>Name</label>
-                          <input placeholder='File Name' type='field' />
-                        </Form.Field>
-
-
-
-
-                          <Form.Field>
+          <Form>
+              <Form.Field>
+                <label>Name</label>
+                <input placeholder='File Name' type='field' name="name" onChange={onChangeHandlerName} value={this.state.path} />
+              </Form.Field>
+              <Form.Field>
 
                           <label>Upload Poster</label>
                              <input
@@ -47,7 +109,7 @@ let onChangeHandler =event=> {
                                name="files"
                                type="file"
 
-                               onChange={onChangeHandler}
+                               onChange={onChangeHandlerPoster}
                              />
 
 
@@ -64,7 +126,7 @@ let onChangeHandler =event=> {
                              name="files"
                              type="file"
 
-                             onChange={onChangeHandler}
+                             onChange={onChangeHandlerVideo}
                            />
 
 
@@ -72,7 +134,7 @@ let onChangeHandler =event=> {
                       </Form.Field>
 
 
-                        <Button primary type='submit' >Submit</Button>
+                        <Button primary type='submit' onClick={submitHondler} >Submit</Button>
 
                         <br/>
                         <br/>
