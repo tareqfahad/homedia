efc1fee387bb02136656da0bd8d9c46a35e9f391
Abdulmajed Dhelaimi <adhelaim@gmail.com>
2019-12-17 18:20:03 +0300

    authentication , login and signup end and frontend

--- homedia-front/src/component/home/VideoPlayContainer.jsx
+++ homedia-front/src/component/home/VideoPlayContainer.jsx
@@ -1,26 +1,15 @@
-// import React from 'react'
-// import {Card, Button, Col} from 'react-bootstrap';
-
-// const VideoPlayContainer = (props) => {
-//     console.log(props.video)
-//     return (
-//         <div>
-//            {props.video.title}
-//         </div>
-//     )
-// }
-
-// export default VideoPlayContainer
 import React from 'react';
+import './assets/homestyle.css'
 import { Player } from 'video-react';
 
 export default props => {
+  
   return (
-    <div style={{"width" : "50%"}}>
+    <div className="vidContainer">
     <Player
       playsInline
       poster="/assets/poster.png"
-      src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+      src={"https://media.w3.org/2010/05/sintel/trailer_hd.mp4"}
     />
     </div>
   );
