6960a3c998b8e486c88a0416eae7b5c0d31d4898
Abdulmajed Dhelaimi <adhelaim@gmail.com>
2019-12-19 09:00:06 +0300

    allvideo and allmusic

--- homedia-front/src/component/home/AllMusic.jsx
+++ homedia-front/src/component/home/AllMusic.jsx
@@ -1,13 +1,44 @@
 import NavBar from './NavBar'
 import MusicCard from './MusicCard'
 import React, { Component } from 'react'
+import axios from 'axios'
+
 
 export default class AllMusic extends Component {
+
+
+    state={music:null}
+
+
+    componentDidMount(){
+
+     
+        axios.get('http://localhost:62300/api/show/music')
+        .then(data2 => {
+            console.log(data2)
+          this.setState({music:data2.data})
+        });
+          
+        
+    }
+
+
+
+
     render() {
         return (
             <div>
                 <NavBar/>
-            <h1 style={{"textAlign":"center"}}>Comming Soon</h1>
+            
+                {this.state.music !== null && this.state.music.map((cards, i)=>{
+                return <MusicCard {...this.props} music={cards} id={cards._id}/>
+              })}
+
+              
+
+
+
+
             </div>
         )
     }
